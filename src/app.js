2f024f69109b7899a62bbbcd0c04b6b50d988557
Nishan Singh <nishansinghthakuri123@gmail.com>
2025-12-25 14:39:47 +0545

    template engine

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import bodyParser from "body-parser";
 import express from "express";
 import multer from "multer";
 
-import { ROLE_ADMIN } from "./constants/roles.js";
 import auth from "./middlewares/auth.js";
 import authRoute from "./routes/auth.route.js";
 import config from "./config/config.js";
-import connectDB from "./config/database.js";
 import connectCloudinary from "./config/cloudinary.js";
+import connectDB from "./config/database.js";
 import logger from "./middlewares/logger.js";
 import orderRoute from "./routes/order.route.js";
+import pageRoute from "./routes/page.route.js";
 import productRoute from "./routes/product.route.js";
 import userRoute from "./routes/user.route.js";
 
@@ -24,6 +24,8 @@ app.use(bodyParser.json());
 
 app.use(logger);
 
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
     res.json({
         name: config.name,
@@ -37,6 +39,7 @@ app.use("/api/products", upload.array("images", 5), productRoute);
 app.use("/api/users", auth, upload.single("image"), userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/orders", auth, orderRoute);
+app.use("/pages", pageRoute);
 
 app.listen(config.port, () => {
     console.log(`Server is running at port: ${config.port}...`);
