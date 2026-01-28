9061c854b69108b64173d066d609059289de7721
Nishan Singh <nishansinghthakuri123@gmail.com>
2026-01-28 14:45:39 +0545

    some changes

--- src/libs/schemas/product.js
+++ src/libs/schemas/product.js
@@ -1,17 +1,20 @@
 import z from "zod";
 
 const productSchema = z.object({
-    name: z.string({ error: "Product name is required." }).min(2),
-    brand: z.string().optional(),
-    category: z.string().optional(),
-    price: z.string({
-        error: (value) =>
-            value === undefined
-                ? "Product price is required."
-                : "Price must be number.",
-    }).min(1).max(9999999),
-    stock: z.number().min(1).optional(),
-    imageUrls: z.array(z.string()).optional(),
+  name: z.string({ error: "Product name is required." }).min(2),
+  brand: z.string().optional(),
+  category: z.string().optional(),
+  price: z
+    .string({
+      error: (value) =>
+        value === undefined
+          ? "Product price is required."
+          : "Price must be number.",
+    })
+    .min(1)
+    .max(9999999),
+  stock: z.string("Stock must be number.").min(1).optional(),
+  imageUrls: z.array(z.string()).optional(),
 });
 
 export { productSchema };
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,23 +1,31 @@
 import { verifyJWT } from "../utils/jwt.js";
 
 const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  let token;
+
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  } else {
     const cookie = req.headers.cookie;
+    
+    if (!cookie) return res.status(401).send("User not authenticated.");
 
-    if(!cookie) return res.status(401).send("User not authenticated.");
+    token = cookie.split("=")[1];
+  }
 
-    const token = cookie.split("=")[1];
+  if (!token) return res.status(401).send("User not authenticated.");
 
-    if(!token) return res.status(401).send("User not authenticated.");
+  try {
+    const data = await verifyJWT(token);
 
-    try {
-        const data = await verifyJWT(token);
+    req.user = data;
 
-        req.user = data; 
-
-        next(); 
-    } catch (error) {
-        res.status(401).send("Invalid token.");
-    }
+    next();
+  } catch (error) {
+    res.status(401).send("Invalid token.");
+  }
 };
 
 export default auth;
