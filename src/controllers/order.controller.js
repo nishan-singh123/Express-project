55aa05d8b148383bce2bb872e13a5684f1944f7c
Nishan Singh <nishansinghthakuri123@gmail.com>
2026-02-20 11:31:44 +0545

    feat: get orders by status

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,130 +1,149 @@
 import orderService from "../services/order.service.js";
 
 const getOrders = async (req, res) => {
-    try {
-        const data = await orderService.getOrders();
+  const status = req.query?.status;
 
-        res.json(data);
-    } catch (error) {
-        res.status(400).send(error?.message);
-    }
+  try {
+    const data = await orderService.getOrders(status);
+
+    res.json(data);
+  } catch (error) {
+    res.status(400).send(error?.message);
+  }
 };
 
 const getOrdersByUser = async (req, res) => {
-    try {
-        const data = await orderService.getOrdersByUser(req.user._id);
+  const status = req.query?.status;
+  
+  try {
+    const data = await orderService.getOrdersByUser(status, req.user._id);
 
-        res.json(data);
-    } catch (error) {
-        res.status(400).send(error?.message);
-    }
+    res.json(data);
+  } catch (error) {
+    res.status(400).send(error?.message);
+  }
 };
 
 const getOrderById = async (req, res) => {
-    try {
-        const data = await orderService.getOrderById(req.params.id);
+  try {
+    const data = await orderService.getOrderById(req.params.id);
 
-        res.json(data);
-    } catch (error) {
-        res.status(error.status || 400).send(error?.message);
-    }
+    res.json(data);
+  } catch (error) {
+    res.status(error.status || 400).send(error?.message);
+  }
 };
 
 const createOrder = async (req, res) => {
-    try {
-        const data = await orderService.createOrder(req.body, req.user._id);
+  try {
+    const data = await orderService.createOrder(req.body, req.user._id);
 
-        res.status(201).json(data);
-    } catch (error) {
-        res.status(400).send(error?.message);
-    }
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(400).send(error?.message);
+  }
 };
 
 const updateOrderStatus = async (req, res) => {
-    try {
-        const data = await orderService.updateOrderStatus(req.params.id, req.body?.status);
+  try {
+    const data = await orderService.updateOrderStatus(
+      req.params.id,
+      req.body?.status,
+    );
 
-        res.json(data);
-    } catch (error) {
-        res.status(error.status || 400).send(error?.message);
-    }
+    res.json(data);
+  } catch (error) {
+    res.status(error.status || 400).send(error?.message);
+  }
 };
 
 const cancelOrder = async (req, res) => {
-    try {
-        const data = await orderService.cancelOrder(req.params.id, req.user);
+  try {
+    const data = await orderService.cancelOrder(req.params.id, req.user);
 
-        res.status(201).json(data);
-    } catch (error) {
-        res.status(error.status || 400).send(error?.message);
-    }
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(error.status || 400).send(error?.message);
+  }
 };
 
 const deleteOrder = async (req, res) => {
-    try {
-        const data = await orderService.deleteOrder(req.params.id);
+  try {
+    const data = await orderService.deleteOrder(req.params.id);
 
-        res.json(data);
-    } catch (error) {
-        res.status(error.status || 400).send(error?.message);
-    }
+    res.json(data);
+  } catch (error) {
+    res.status(error.status || 400).send(error?.message);
+  }
 };
 
 const orderPaymentViaKhalti = async (req, res) => {
-    try {
-        const data = await orderService.orderPaymentViaKhalti(req.params.id);
+  try {
+    const data = await orderService.orderPaymentViaKhalti(req.params.id);
 
-        res.json(data);
-    } catch (error) {
-        res.status(error.status || 400).send(error?.message);
-    }
+    res.json(data);
+  } catch (error) {
+    res.status(error.status || 400).send(error?.message);
+  }
 };
 
 const orderPaymentViaStripe = async (req, res) => {
-    try {
-        const data = await orderService.orderPaymentViaStripe(req.params.id);
+  try {
+    const data = await orderService.orderPaymentViaStripe(req.params.id);
 
-        res.json(data);
-    } catch (error) {
-        res.status(error.status || 400).send(error?.message);
-    }
+    res.json(data);
+  } catch (error) {
+    res.status(error.status || 400).send(error?.message);
+  }
 };
 
 const orderPaymentViaCash = async (req, res) => {
-    try {
-        const data = await orderService.orderPaymentViaCash(req.params.id);
+  try {
+    const data = await orderService.orderPaymentViaCash(req.params.id);
 
-        res.json(data);
-    } catch (error) {
-        res.status(error.status || 400).send(error?.message);
-    }
+    res.json(data);
+  } catch (error) {
+    res.status(error.status || 400).send(error?.message);
+  }
 };
 
 const confirmOrderPayment = async (req, res) => {
-    if (!req.body?.status)
-        return res.status(400).send("Payment status is required.");
+  if (!req.body?.status)
+    return res.status(400).send("Payment status is required.");
 
-    try {
-        const data = await orderService.confirmOrderPayment(
-            req.params.id,
-            req.body.status
-        );
+  try {
+    const data = await orderService.confirmOrderPayment(
+      req.params.id,
+      req.body.status,
+    );
 
-        res.json(data);
-    } catch (error) {
-        res.status(error.status || 400).send(error?.message);
-    }
+    res.json(data);
+  } catch (error) {
+    res.status(error.status || 400).send(error?.message);
+  }
 };
 
 const getOrdersByMerchant = async (req, res) => {
-    try {
-        const data = await orderService.getOrdersByMerchant(req.user._id);
+  try {
+    const data = await orderService.getOrdersByMerchant(req.user._id);
 
-        res.json(data);
-    } catch (error) {
-        res.status(error.status || 400).send(error?.message);
-    }
+    res.json(data);
+  } catch (error) {
+    res.status(error.status || 400).send(error?.message);
+  }
 };
 
-
-export default { createOrder, getOrders, getOrdersByUser, cancelOrder, deleteOrder, getOrderById, updateOrderStatus, orderPaymentViaKhalti, orderPaymentViaCash, confirmOrderPayment, getOrdersByMerchant, orderPaymentViaStripe };
+export default {
+  createOrder,
+  getOrders,
+  getOrdersByUser,
+  cancelOrder,
+  deleteOrder,
+  getOrderById,
+  updateOrderStatus,
+  orderPaymentViaKhalti,
+  orderPaymentViaCash,
+  confirmOrderPayment,
+  getOrdersByMerchant,
+  orderPaymentViaStripe,
+};
--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,182 +1,212 @@
 import crypto from "crypto";
 
-import { ORDER_STATUS_CANCELLED, ORDER_STATUS_CONFIRMED } from "../constants/orderStatuses.js";
+import {
+  ORDER_STATUS_CANCELLED,
+  ORDER_STATUS_CONFIRMED,
+} from "../constants/orderStatuses.js";
 import { payViaKhalti, payViaStripe } from "../utils/payment.js";
 import { ROLE_ADMIN } from "../constants/roles.js";
 import Order from "../models/Order.js";
 import Payment from "../models/Payment.js";
 import mongoose from "mongoose";
 
-const getOrders = async () => {
-    return await Order.find().sort({ createdAt: -1 }).populate("user", "name email phone").populate("orderItems.product", "name brand category price imageUrls");
+const getOrders = async (status) => {
+  return await Order.find({ status })
+    .sort({ createdAt: -1 })
+    .populate("user", "name email phone")
+    .populate("orderItems.product", "name brand category price imageUrls");
 };
 
-const getOrdersByUser = async (userId) => {
-    return await Order.find({ user: userId })
-        .sort({ createdAt: -1 })
-        .populate("user", "name email phone")
-        .populate("orderItems.product", "name brand category price imageUrls");
+const getOrdersByUser = async (status, userId) => {
+    let filter = {user: userId};
+
+    if(status) filter.status = status;
+
+  return await Order.find(filter)
+    .sort({ createdAt: -1 })
+    .populate("user", "name email phone")
+    .populate("orderItems.product", "name brand category price imageUrls");
 };
 
 const getOrderById = async (id) => {
-    const order = await Order.findById(id)
-        .populate("user", "name email phone")
-        .populate("orderItems.product", "name brand category price imageUrls");
+  const order = await Order.findById(id)
+    .populate("user", "name email phone")
+    .populate("orderItems.product", "name brand category price imageUrls");
 
-    if (!order)
-        throw {
-            status: 404,
-            message: "Order not found.",
-        };
+  if (!order)
+    throw {
+      status: 404,
+      message: "Order not found.",
+    };
 
-    return order;
+  return order;
 };
 
 const createOrder = async (data, userId) => {
-    const orderNumber = crypto.randomUUID();
+  const orderNumber = crypto.randomUUID();
 
-    return await Order.create({ ...data, user: userId, orderNumber });
+  return await Order.create({ ...data, user: userId, orderNumber });
 };
 
 const updateOrderStatus = async (id, status) => {
-    return await Order.findByIdAndUpdate(id, { status }, { new: true });
+  return await Order.findByIdAndUpdate(id, { status }, { new: true });
 };
 
 const cancelOrder = async (id, user) => {
-    const order = await getOrderById(id);
+  const order = await getOrderById(id);
 
-    if (!user.roles.includes(ROLE_ADMIN) && order.user._id != user._id)
-        throw {
-            status: 403,
-            message: "Access denied.",
-        };
+  if (!user.roles.includes(ROLE_ADMIN) && order.user._id != user._id)
+    throw {
+      status: 403,
+      message: "Access denied.",
+    };
 
-    return await Order.findByIdAndUpdate(
-        id,
-        { status: ORDER_STATUS_CANCELLED },
-        { new: true }
-    );
+  return await Order.findByIdAndUpdate(
+    id,
+    { status: ORDER_STATUS_CANCELLED },
+    { new: true },
+  );
 };
 
 const deleteOrder = async (id) => {
-    await getOrderById(id);
+  await getOrderById(id);
 
-    return await Order.findByIdAndDelete(id);
+  return await Order.findByIdAndDelete(id);
 };
 
 const orderPaymentViaKhalti = async (id) => {
-    const order = await getOrderById(id);
+  const order = await getOrderById(id);
 
-    const orderPayment = await Payment.create({
-        method: "ONLINE",
-        amount: order.totalPrice,
-    });
+  const orderPayment = await Payment.create({
+    method: "ONLINE",
+    amount: order.totalPrice,
+  });
 
-    await Order.findByIdAndUpdate(id, {
-        payment: orderPayment._id,
-    });
+  await Order.findByIdAndUpdate(id, {
+    payment: orderPayment._id,
+  });
 
-    return await payViaKhalti({
-        id,
-        amount: order.totalPrice,
-        purchaseOrderId: order.orderNumber,
-        purchaseOrderName: order.orderItems[0].product.name,
-        customer: order.user,
-    });
+  return await payViaKhalti({
+    id,
+    amount: order.totalPrice,
+    purchaseOrderId: order.orderNumber,
+    purchaseOrderName: order.orderItems[0].product.name,
+    customer: order.user,
+  });
 };
 
 const orderPaymentViaStripe = async (id) => {
-    const order = await getOrderById(id);
+  const order = await getOrderById(id);
 
-    const orderPayment = await Payment.create({
-        method: "CARD",
-        amount: order.totalPrice,
-    });
+  const orderPayment = await Payment.create({
+    method: "CARD",
+    amount: order.totalPrice,
+  });
 
-    await Order.findByIdAndUpdate(id, {
-        payment: orderPayment._id,
-    });
+  await Order.findByIdAndUpdate(id, {
+    payment: orderPayment._id,
+  });
 
-    return await payViaStripe({
-        amount: order.totalPrice,
-        orderId: order.orderNumber,
-        orderName: order.orderItems[0].product.name,
-        customer: order.user,
-    });
+  return await payViaStripe({
+    amount: order.totalPrice,
+    orderId: order.orderNumber,
+    orderName: order.orderItems[0].product.name,
+    customer: order.user,
+  });
 };
 
 const orderPaymentViaCash = async (id) => {
-    const order = await getOrderById(id);
+  const order = await getOrderById(id);
 
-    const orderPayment = await Payment.create({
-        method: "CASH",
-        amount: order.totalPrice,
-    });
+  const orderPayment = await Payment.create({
+    method: "CASH",
+    amount: order.totalPrice,
+  });
 
-    return await Order.findByIdAndUpdate(id, {
-        payment: orderPayment._id,
-        status: ORDER_STATUS_CONFIRMED,
-    }, { new: true }
-    );
+  return await Order.findByIdAndUpdate(
+    id,
+    {
+      payment: orderPayment._id,
+      status: ORDER_STATUS_CONFIRMED,
+    },
+    { new: true },
+  );
 };
 
 const confirmOrderPayment = async (id, status) => {
-    const order = await getOrderById(id);
+  const order = await getOrderById(id);
 
-    if (status.toUpperCase() !== "COMPLETED") {
-        await Payment.findByIdAndUpdate(order.payment, {
-            status: "FAILED",
-        });
-
-        throw {
-            status: 400,
-            message: "Payment failed.",
-        };
-    }
-
+  if (status.toUpperCase() !== "COMPLETED") {
     await Payment.findByIdAndUpdate(order.payment, {
-        status: "SUCCESS",
+      status: "FAILED",
     });
 
-    return await Order.findByIdAndUpdate(id, {
-        status: ORDER_STATUS_CONFIRMED,
-    }, { new: true });
+    throw {
+      status: 400,
+      message: "Payment failed.",
+    };
+  }
+
+  await Payment.findByIdAndUpdate(order.payment, {
+    status: "SUCCESS",
+  });
+
+  return await Order.findByIdAndUpdate(
+    id,
+    {
+      status: ORDER_STATUS_CONFIRMED,
+    },
+    { new: true },
+  );
 };
 
 const getOrdersByMerchant = async (merchantId) => {
-    return await Order.aggregate([
-        {
-            $lookup: {
-                from: "products",
-                localField: "orderItems.product",
-                foreignField: "_id",
-                as: "orderedProducts",
-            },
-        },
-        {
-            $unwind: "$orderedProducts",
-        },
-        {
-            $match: {
-                "orderedProducts.createdBy": new mongoose.Types.ObjectId(merchantId),
-            },
-        },
-        {
-            $project: {
-                orderNumber: 1,
-                payment: 1,
-                shippingAddress: 1,
-                status: 1,
-                totalPrice: 1,
-                user: 1,
-                "orderedProducts.price": 1,
-                "orderedProducts.name": 1,
-                "orderedProducts.imageUrls": 1,
-                "orderedProducts.category": 1,
-                "orderedProducts.brand": 1,
-            },
-        },
-    ]);
+  return await Order.aggregate([
+    {
+      $lookup: {
+        from: "products",
+        localField: "orderItems.product",
+        foreignField: "_id",
+        as: "orderedProducts",
+      },
+    },
+    {
+      $unwind: "$orderedProducts",
+    },
+    {
+      $match: {
+        "orderedProducts.createdBy": new mongoose.Types.ObjectId(merchantId),
+      },
+    },
+    {
+      $project: {
+        orderNumber: 1,
+        payment: 1,
+        shippingAddress: 1,
+        status: 1,
+        totalPrice: 1,
+        user: 1,
+        "orderedProducts.price": 1,
+        "orderedProducts.name": 1,
+        "orderedProducts.imageUrls": 1,
+        "orderedProducts.category": 1,
+        "orderedProducts.brand": 1,
+      },
+    },
+  ]);
 };
 
-export default { createOrder, getOrders, getOrdersByUser, cancelOrder, deleteOrder, getOrderById, updateOrderStatus, orderPaymentViaKhalti, confirmOrderPayment, orderPaymentViaCash, getOrdersByMerchant, orderPaymentViaStripe };
+export default {
+  createOrder,
+  getOrders,
+  getOrdersByUser,
+  cancelOrder,
+  deleteOrder,
+  getOrderById,
+  updateOrderStatus,
+  orderPaymentViaKhalti,
+  confirmOrderPayment,
+  orderPaymentViaCash,
+  getOrdersByMerchant,
+  orderPaymentViaStripe,
+};
